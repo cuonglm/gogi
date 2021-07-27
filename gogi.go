4fb12580dee90ad06b1685e0f6c76dd47bd55b3e
Nguyễn Quang Minh <39944621+VTCANguyenQuangMinh@users.noreply.github.com>
2021-07-27 13:47:15 +0700

    Update default API  server url (#7)
    Base on gitignore documentation, the API server url is changed from www.gitignore.io to
    www.toptal.com domain, so use that url instead.
    
    See: https://docs.gitignore.io/install/command-line
    
    Fixes #1

--- gogi.go
+++ gogi.go
@@ -9,8 +9,8 @@ import (
 const (
 	version       = "0.0.4"
 	ua            = "gogi/" + version
-	defaultAPIURL = "https://www.gitignore.io"
-	typePath      = "/api"
+	defaultAPIURL = "https://www.toptal.com"
+	typePath      = "/developers/gitignore/api"
 )
 
 // Client for querying API
